406c0f09033564c6f887f6e6b9a28a6b2d7893e3
Jacob Menashi <jbmenashi@gmail.com>
2018-10-02 17:11:30 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,7 @@
 def reverse_each_word(sentence)
   array_sentence = sentence.split(" ") 
   array_sentence.each do |word|
-    word.reverse 
+    word.reverse_each 
   end
   array_sentence.join(" ")
 end
