80a0d37f7cc8ff0b1bf1ab55158a7995cc62f703
Jacob Menashi <jbmenashi@gmail.com>
2018-10-02 17:15:59 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,11 +1,11 @@
-def reverse_each_word(sentence)
-  array_sentence = sentence.split(" ")
-  reversed_array = []
-  array_sentence.each do |word|
-    reversed_array << word.reverse 
-  end
-  reversed_array.join(" ")
-end
+# def reverse_each_word(sentence)
+#   array_sentence = sentence.split(" ")
+#   reversed_array = []
+#   array_sentence.each do |word|
+#     reversed_array << word.reverse 
+#   end
+#   reversed_array.join(" ")
+# end
 
 def reverse_each_word(sentence)
   array_sentence = sentence.split(" ")
